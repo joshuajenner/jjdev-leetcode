638c3497c975bbc73d3702bfe4eb6478179f7adc
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-09-22 16:49:06 +0800

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+// typically using all the original letters exactly once.
+
+bool isAnagram(string, string);
+
+
+bool isAnagram(string s, string t)
+{
+    if (s.length() == t.length())
+    {
+        unordered_map < char, int > countMap;
+        for (int index = 0; index < s.length(); index++)
+        {
+            countMap[s[index]]++;
+            countMap[t[index]]--;
+        }
+        for (auto i = countMap.begin(); i != countMap.end(); i++)
+        {
+            if (i->second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
