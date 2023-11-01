d3db8e27c3d8051978f660e46c9de4b3dc95a1c2
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-11-01 18:11:52 +0800

    Update 54. Spiral Matrix.cpp

--- 54. Spiral Matrix.cpp
+++ 54. Spiral Matrix.cpp
@@ -7,57 +7,52 @@ vector<int> spiralOrder(vector<vector<int>>& matrix);
 
 vector<int> spiralOrder(vector<vector<int>>& matrix)
 {
-	enum Direction {goingLeft, goingDown, goingRight, goingUp};
-	Direction direction = goingLeft;
+	vector<int> matrixSpiral = {};
+	
+	if (matrix.empty() || matrix[0].empty())
+	{
+		return matrixSpiral;
+	}
 
+	int rows = matrix.size();
+	int columns = matrix[0].size();
+	
 	int topBorder = 0;
-	int rightBorder = 0;
-	int bottomBorder = 0;
+	int rightBorder = columns - 1;
+	int bottomBorder = rows - 1;
 	int leftBorder = 0;
 
-	int count = matrix[0].size() * matrix.size();
-	vector<int> matrixSpiral = {};
-	vector<int> position = { 0, 0 };
 
-	for (int index = 0; index < count; count++) 
+	while (leftBorder <= rightBorder && topBorder <= bottomBorder)
 	{
-		matrixSpiral.push_back(matrix[position[0]][position[1]]);
-
-		// update position and direction
-		switch (direction)
+		for (int index = leftBorder; index <= rightBorder; index++)
 		{
-			case goingLeft:
-				position[0] += 1;
-				if (position[0] == (matrix[0].size() - rightBorder))
-				{
-					direction = goingDown;
-					topBorder += 1;
-				}
-				break;
-			case goingDown:
-				position[1] += 1;
-				if (position[1] == (matrix.size() - bottomBorder))
-				{
-					direction = goingRight;
-					rightBorder += 1;
-				}
-				break;
-			case goingRight:
-				position[0] -= 1;
-				if (position[0] == (matrix[0].size() - leftBorder))
-				{
-					direction = goingUp;
-					bottomBorder += 1;
-				}
-				break;
-			case goingUp:
-				position[1] += 1;
-				if (position[1] == (matrix.size() - topBorder))
-				{
-					direction = goingLeft;
-					leftBorder += 1;
-				}
-				break;
+			matrixSpiral.push_back(matrix[topBorder][index]);
+		}
+		topBorder++;
+
+		for (int index = topBorder; index <= bottomBorder; index++)
+		{
+			matrixSpiral.push_back(matrix[index][rightBorder]);
+		}
+		rightBorder--;
+
+		if (topBorder <= bottomBorder)
+		{
+			for (int index = rightBorder; index >= leftBorder; index--)
+			{
+				matrixSpiral.push_back(matrix[bottomBorder][index]);
+			}
+			bottomBorder--;
+		}
+
+		if (leftBorder <= rightBorder)
+		{
+			for (int index = bottomBorder; index >= topBorder; index--)
+			{
+				matrixSpiral.push_back(matrix[index][leftBorder]);
+			}
+			leftBorder++;
 		}
 	}
 	return matrixSpiral;
