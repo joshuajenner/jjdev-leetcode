cc49bfac7e2d4ce0979d45707a4d4b8b1896347e
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2024-01-18 20:43:21 +0800

    Update 70. Climbing Stairs.cpp

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,29 +1,23 @@
-//You are climbing a staircase.It takes n steps to reach the top.
+// You are climbing a staircase.It takes n steps to reach the top.
 //
-//Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top?
+// Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top?
 
-class Solution {
+class Solution
+{
 public:
-    int climbStairs(int n) {
-        int ways = 1;
-        int numOf2s = 1;
+    int climbStairs(int n)
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
 
-        for (int index = n - 1; index >= 1; index--) {
-            ways += combinations(numOf2s, index);
-            numOf2s++;
+        for (int i = 0; i < n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
         }
 
-        return ways;
-    }
-
-    int combinations(int pick, int total) {
-        return factorial(total) / factorial(total - pick) * factorial(pick);
-    }
-
-    int factorial(int num) {
-        if (num <= 1) {
-            return 1;
-        }
-        return num * factorial(num - 1);
+        return b;
     }
 };
